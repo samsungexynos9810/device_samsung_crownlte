d90dbeb493f87b6278f7e5597d2d5fbd33ddb568
Dylan Neve <dylanneve1@gmail.com>
2022-08-22 11:30:17 +0100

    crownlte: Bring-up for AwakenOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crownlte.mk
+    $(LOCAL_DIR)/awaken_crownlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_crownlte-eng \
-    aosp_crownlte-user \
-    aosp_crownlte-userdebug
+    awaken_crownlte-eng \
+    awaken_crownlte-user \
+    awaken_crownlte-userdebug
